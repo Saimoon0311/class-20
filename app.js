f670ec37f1a92b67118f0e0caf0f7e7ff11a4d87
unknown <you@example.com>
2021-06-18 19:03:41 +0500

    class 20

---
+++ app.js
@@ -0,0 +1,37 @@
+// function check(){
+//     var score = 0
+//     var right_answer_1 = document.getElementById("q1-a1")
+//     var q1_answer_2 = document.getElementById("q1-a2")
+//     var q1_answer_3 = document.getElementById("q1-a3")
+//     var q1_answer_4  = document.getElementById("q1-a4")
+//     if(right_answer_1.checked == true){
+//         score ++
+//         alert("right")
+//     }else{
+//         alert("wrong")
+//     }
+
+//     // var score = 0
+//     var right_answer_2 = document.getElementById("q2-a1")
+//     var q2_answer_2 = document.getElementById("q2-a2")
+//     var q2_answer_3 = document.getElementById("q2-a3")
+//     var q2_answer_4  = document.getElementById("q2-a4")
+//     if(right_answer_2.checked == true){
+//         score ++
+//         alert("right")
+//     }else{
+//         alert("wrong")
+//     }
+
+//     // var score = 0
+//     var right_answer_3 = document.getElementById("q3-a1")
+//     var q3_answer_2 = document.getElementById("q3-a2")
+//     var q3_answer_3 = document.getElementById("q3-a3")
+//     var q3_answer_4  = document.getElementById("q3-a4")
+//     if(right_answer_3.checked == true){
+//         score ++
+//         alert("right")
+//     }else{
+//         alert("wrong")
+//     }
+// }
